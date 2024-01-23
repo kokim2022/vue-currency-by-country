3c89d9d84c85ab2653b90f30ff963b6535036979
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-01-24 01:59:20 +0630

    dfds

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@ import { defineConfig } from "vite";
 import path, { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+// import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dts(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: "node_modules/vue-multiselect/dist/vue-multiselect.css",
-        dest: "./",
-        rename: 'vue-currency-by-country.css'
-      }// Provide the desired name
-    ],
-  })
+  // viteStaticCopy({
+  //   targets: [
+  //     {
+  //       src: "node_modules/vue-multiselect/dist/vue-multiselect.css",
+  //       dest: "./",
+  //       rename: 'vue-currency-by-country.css'
+  //     }// Provide the desired name
+  //   ],
+  // })
   ],
 
   build: {

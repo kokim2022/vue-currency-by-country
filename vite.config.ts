7edd2cc8aff1cec49efb01f8f9b4e65653608b53
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-01-24 02:09:46 +0630

    fdsf

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,27 @@ import { defineConfig } from "vite";
 import path, { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
-// import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dts(),
-  // viteStaticCopy({
-  //   targets: [
-  //     {
-  //       src: "node_modules/vue-multiselect/dist/vue-multiselect.css",
-  //       dest: "./",
-  //       rename: 'vue-currency-by-country.css'
-  //     }// Provide the desired name
-  //   ],
-  // })
+  viteStaticCopy({
+    targets: [
+      {
+        src: "node_modules/vue-multiselect/dist/vue-multiselect.css",
+        dest: "./",
+        rename: 'vue-currency-by-country.css'
+      }// Provide the desired name
+    ],
+  })
   ],
 
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "VueCurrencyByCountry",
-      fileName: "vue-currency-by-country",
+      fileName: "vue-currency-by-country"
     },
     rollupOptions: {
       external: ["vue"],
